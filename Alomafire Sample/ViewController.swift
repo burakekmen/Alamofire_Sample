952543394af22e907b37dcea1237874df4fb8a17
Burak Ekmen <ekmen.burak@hotmail.com>
2020-05-26 13:14:17 +0300

    first commit

--- Alomafire Sample/ViewController.swift
+++ Alomafire Sample/ViewController.swift
@@ -7,14 +7,23 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        sendRequest()
     }
 
+    
+    func sendRequest(){
+        AF.request("https://httpbin.org/get").response{
+            response in debugPrint(response)
+        }
+    }
 
 }
 
