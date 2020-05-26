9f61692fd4274078de78458c83ad3400ab217992
Burak Ekmen <ekmen.burak@hotmail.com>
2020-05-26 13:00:55 +0300

    Initial Commit

---
+++ Alomafire Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alomafire Sample
+//
+//  Created by BURAK EKMEN on 26.05.2020.
+//  Copyright © 2020 Burak Ekmen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
